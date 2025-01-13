23a39615f484081a2a86c84a79c2ae599484d880
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-01-13 11:32:31 +0000

    conversionOPeration

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all JS code as newer version
+
+//alret(3+3) we are using node js ,not browser
+
+
+let name="Pushpesh"
+let age=45
+let isloggedIn=false
+
+// number=>2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=> standalone value
+//undefined=>
+//symbol =>unique
+
+
+//object
+
+console.log(typeof "Pushpesh")
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,45 @@
+
+
+// let score="33"
+// console.log(score)
+// console.log(typeof score)
+// console.log(typeof(score))
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+
+// let x=null
+// console.log(x)
+// console.log(typeof(x))
+// valueInNumber=Number(x)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+
+
+// let y
+// console.log(y)
+// console.log(typeof(y))
+// valueInNumber=Number(y)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// let numstr="7mf"
+// console.log(numstr)
+// console.log(typeof(numstr))
+// valueInNumber=Number(numstr)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+// let str="Pushpesh"
+// console.log(str)
+// console.log(typeof(str))
+// valueInNumber=Number(str)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+
+ boolean=true
+console.log(boolean)
+
