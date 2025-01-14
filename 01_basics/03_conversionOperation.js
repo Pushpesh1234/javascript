9ae3e1eabc4bcccf649c8acfdbd0774f4062948e
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-01-14 16:16:59 +0000

    Comparsion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,9 @@
 
+     
+// Conversion using Number() Method
 
-// let score="33"
-// console.log(score)
+// let score="33"         "33"=>33
+// console.log(score)       
 // console.log(typeof score)
 // console.log(typeof(score))
 // let valueInNumber=Number(score)
@@ -10,7 +12,7 @@
 
 
 
-// let x=null
+// let x=null             null => value is 0 ; Number() method typeof gives number
 // console.log(x)
 // console.log(typeof(x))
 // valueInNumber=Number(x)
@@ -22,11 +24,11 @@
 // let y
 // console.log(y)
 // console.log(typeof(y))
-// valueInNumber=Number(y)
+// valueInNumber=Number(y)             y undefined =>value is NaN ; type converted in number when used Number() method
 // console.log(typeof(valueInNumber))
 // console.log(valueInNumber)
 
-// let numstr="7mf"
+// let numstr="7mf"       "7mf"=>value given is NaN
 // console.log(numstr)
 // console.log(typeof(numstr))
 // valueInNumber=Number(numstr)
@@ -35,11 +37,74 @@
 
 // let str="Pushpesh"
 // console.log(str)
-// console.log(typeof(str))
+// console.log(typeof(str))    "Pushpesh"=>value is NaN (Not a Number)
 // valueInNumber=Number(str)
 // console.log(typeof(valueInNumber))
 // console.log(valueInNumber)
 
- boolean=true
-console.log(boolean)
+//  let value=true
+// console.log(value)
+// console.log(typeof(value))      true => value is 1
+// let valueInNumber=Number(value)  false =>value is 0
+// console.log(typeof(value))
+// console.log(valueInNumber)
+
+//Conversion using Boolean() Method
+
+// let isLoggedIn=1     //1 => value is true using conversion Method Boolean()
+// console.log(isLoggedIn)    //0=> value is false using conversion method Boolean()
+// let booleanisLoggedIn=Boolean(isLoggedIn) // " "(Empty String) => value is false using conversion method Boolean()
+// console.log(typeof(booleanisLoggedIn))      // "Pushpesh"(String)=>value is true
+// console.log(booleanisLoggedIn)
+
+
+
+// Conversion using String() Method
+
+// let num=33
+// console.log(typeof(num))
+// let stringNum=String(num)
+// console.log(typeof(stringNum))
+// console.log(stringNum)
+
+
+
+//*****************************************OPerations in Javascript**********************************************
+
+let value=3
+let negvalue=-value
+console.log(negvalue);
+console.log(2+2);
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2**4)
+console.log(4/2)
+console.log(4%2)
+
+let str1="Pushpesh"
+let str2=" Rawat"
+let str3=str1+str2
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"2")
+
+console.log(true);
+console.log(+true);
+console.log(true+);
+console.log(+"");
+
+let num1,num2,num3
+num1=num2=num3=2+2// not a good way of writing code readiablity decrease
+
+let gamecounter=100
+gamecounter++;
+console.log(gamecounter)
+++gamecounter
+console.log(gamecounter)
+
+
 
