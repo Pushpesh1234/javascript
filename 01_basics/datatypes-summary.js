a7b21d55874da779ce98c62eb7251d8e71d3393a
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-01-15 07:39:19 +0000

    Datatype Summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+//#Primitive types (value)
+
+// 7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+
+
+// Refrence (Non-Primitive)
+// Array,Functions,Objects
+
+// variable does not have type but value have a type example
+
+//  const score=100 (100 is a number type)
+
+//  const isLoggedIn=false (false is a boolean type)
+
+// let  useEmail;   (undefined is a undefined type)
+// const anotherId= Symbol('123') (Symbol) (Symbol is a symbol type)
+// const Id=Symbol('123')
+// console.log(anotherId===Id);
+
+
+
+// Bigint value type is bigint
+// const bigNumber=1000000000000000n
+// console.log(typeof(bigNumber)) 
+
+// Arrays => value type is object
+const heroes=["Superman","Batman","Ironman"]
+
+// Object => value type is object
+let myObj={
+    name:"Pushpesh",
+    age:23
+ 
+}
+// Function=> type is Function orfunction object
+const myFunction= function(){
+    console.log("Hello World")
+}
