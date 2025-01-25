921092767e40882bdc8652370b123c3c5a9abc56
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-01-25 12:20:24 +0000

    Array1

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,32 @@
+//Array
+
+const myArr=[0,1,2,3,4,5,"Pushpesh",false]
+// console.log(myArr)
+// console.log(myArr[0])
+// myChangeArr=myArr
+// console.log(myChangeArr)
+// myChangeArr.push(6)
+// console.log(myArr)
+
+// Array Methods
+// myArr.push(7)
+// myArr.pop()
+// myArr.unshift(9)
+// myArr.shift()
+// myArr.includes(9)
+// myArr.indexOf(6)
+// console.log(myArr)
+// const newArr=myArr.join()
+// console.log(newArr)
+// console.log(typeof newArr)
+
+console.log("A", myArr)
+const myn1=myArr.slice(1,3)
+console.log(myn1)
+console.log("B",myArr)
+const myn2=myArr.splice(1,3)
+console.log(myn2)
+console.log("C",myArr)
+
+
+
