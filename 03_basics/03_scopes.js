0b640d525c940067b9e6d70e72424c729608a53f
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-01-31 15:24:17 +0000

    About scopes in javascript

---
+++ 03_basics/03_scopes.js
@@ -0,0 +1,64 @@
+ let a= 10;
+// const b=20;
+// var c=30;
+//  console.log(a,b,c)
+
+
+ if(true){
+    let a= 333;
+ const b=20;
+ var c=30;
+ d=4;
+ console.log("INNER:", a)
+ 
+ }
+//  console.log(a);
+//  console.log(b);
+//  console.log(c);
+//  console.log(d);
+
+
+// function one(){
+//    const username="Pushpesh"
+//    function two(){
+//     const website="Github"
+   
+//    console.log(username);
+//    }
+
+//console.log(website)
+//two()
+// }
+// one();
+
+
+// if(true){
+//     const username="Pushpesh"
+//     if(username="Pushpesh"){
+//         const website="Youtube"
+//         console.log(username+website)
+
+//     }
+//     console.log(wesite)
+
+// }
+// console.log(username)
+
+
+// Hoisting in javascript
+ hoisting(34)
+
+function hoisting(num){
+ return num+1
+}
+
+// hoisting(4)
+
+hoisting(89)
+const hoisting2=function(num1){
+    return num1+2
+
+
+}
+
+// hoisting2(4)
