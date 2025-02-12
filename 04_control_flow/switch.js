9ded95025f545141f2fb830bd3bf7104dae961ba
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-02-12 18:04:42 +0000

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,30 @@
+const month=4
+
+switch(month){
+    case 1:
+        console.log("January")
+        break;
+        case 2:
+        console.log("February")
+        break;
+        case 3:
+        console.log("January")
+        break;
+        case 4:
+        console.log("January")
+        break;
+        case 5:
+         console.log("January")
+        break;
+        case 6:
+        console.log("January")
+        break;
+        case 7:
+        console.log("January")
+        break;  
+        default:
+            break;
+}
+
+
+
