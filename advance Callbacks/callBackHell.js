62aa1c7fe2b62040688c9ad4c86a5457e7c0097b
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-03-16 16:17:58 +0000

    call back hell in js

---
+++ advance Callbacks/callBackHell.js
@@ -0,0 +1,56 @@
+
+
+const scriptLoad = function (src, callback) {
+    const script = document.createElement('script')
+    script.src = src
+    script.onload = function () {
+        console.log("Loaded script with src SRC:" + src)
+        callback(null, src)
+
+    }
+
+    script.onerror = function () {
+        callback(new Error("this is a error in src"), "this is not valid src:" + src)
+    }
+    console.log("hello fuckers")
+    document.body.appendChild(script)
+}
+
+
+scriptLoad("https://code.jquery.com/jquery-3.4.1.slim.min.js", function test(error, src) {
+    if (error) {
+        console.log(error)
+        return
+    }
+    scriptLoad("https://code.jquery.com/jquery-3.4.1.slim.min.js", function test(error, src) {
+        if (error) {
+            console.log(error)
+            return
+        }
+        scriptLoad("https://code.jquery.com/jquery-3.4.1.slim.min.js", function test(error, src) {
+            if (error) {
+                console.log(error)
+                return
+            }
+            scriptLoad("https://code.jquery.com/jquery-3.4.1.slim.min.js", function test(error, src) {
+                if (error) {
+                    console.log(error)
+                    return
+                }
+                scriptLoad("https://code.jquery.com/jquery-3.4.1.slim.min.js", function test(error, src) {
+                    if (error) {
+                        console.log(error)
+                        return
+                    }
+                    scriptLoad("https://code.jquery.com/jquery-3.4.1.slim.min.js", function test(error, src) {
+                        if (error) {
+                            console.log(error)
+                            return
+                        }
+
+                    })
+                })
+            })
+        })
+    })
+})
