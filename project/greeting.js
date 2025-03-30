14c86d1fdef5c67ddc2456b973efdeb8e403d16f
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-03-30 18:46:48 +0000

    greeting files dynamic modules

---
+++ project/greeting.js
@@ -0,0 +1,11 @@
+const night=()=> console.log('Good Night');
+
+const evening=() => console.log('Good Evening');
+
+const afternoon=() => console.log('Good Afternon');
+
+const morning=() => console.log('Good Morning');
+
+
+
+export{night,evening,afternoon,morning};
