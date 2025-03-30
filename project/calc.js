e6bac1b6a4bd73ad388b5989a4af0816f603093d
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-03-30 18:45:00 +0000

    exported  script calc file

---
+++ project/calc.js
@@ -0,0 +1,21 @@
+ 
+
+
+export const sum =(a,b) =>{
+   return a+b;
+}
+
+export const sub=(a,b) =>{
+    return a-b;
+}
+
+
+const mulp=(a,b)=>{
+    return a*b;
+}
+
+const power=(a,b) => a**b;
+
+export{mulp};
+
+export default power;
