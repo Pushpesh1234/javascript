88f955a1da32c9fe6354c42020dfc6d6e020cc9b
Pushpesh1234 <pushpeshrawat2001@gmail.com>
2025-04-27 17:37:26 +0000

    same as before

--- promises/promiseHitesh.js
+++ promises/promiseHitesh.js
@@ -1,49 +1,64 @@
-const promise=new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-    const error=true
-    if(!error){
-        console.log("data is resolved")
-        resolve({name:"Pushpesh", email:"rawat2001@gmail.com"})
-    }
-    else{
-        console.log("data is rejected")
-        reject( "Something went wrong")
-    }
+// const promise=new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//     const error=true
+//     if(!error){
+//         console.log("data is resolved")
+//         resolve({name:"Pushpesh", email:"rawat2001@gmail.com"})
+//     }
+//     else{
+//         console.log("data is rejected")
+//         reject( "Something went wrong")
+//     }
    
-    },1000)
-})
+//     },1000)
+// })
 
-promise.then((user)=>{
-    return user.username
-}).then((username)=>{
-    console.log(username)
-})
-.catch((error)=>{
-    console.log(error)
-}).finally(()=> console.log("rejcted  or resolved message finally"))
+// promise.then((user)=>{
+//     return user.username
+// }).then((username)=>{
+//     console.log(username)
+// })
+// .catch((error)=>{
+//     console.log(error)
+// }).finally(()=> console.log("rejcted  or resolved message finally"))
     
 
 
-    const promiseTwo=new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-        const error=false
-        if(!error){
+//     const promiseTwo=new Promise((resolve, reject)=>{
+//         setTimeout(()=>{
+//         const error=false
+//         if(!error){
             
-            console.log("data is resolved")
-            resolve({name:"Pushpesh", email:"rawat2001@gmail.com"})
-        }
-        else{
-            console("data is rejected")
-            reject(   Error("Something went wrong"))
-        }
+//             console.log("data is resolved")
+//             resolve({name:"Pushpesh", email:"rawat2001@gmail.com"})
+//         }
+//         else{
+//             console("data is rejected")
+//             reject(   Error("Something went wrong"))
+//         }
        
-        },1000)
-    })
+//         },1000)
+//     })
     
-promiseTwo.then((user)=>{
-    console.log(user)
-    return user.name
+// promiseTwo.then((user)=>{
+//     console.log(user)
+//     return user.name
 
-}).then((username)=>{
-    console.log(username)
+// }).then((username)=>{
+//     console.log(username)
+// })
+
+const promiseThree=new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve("message is resolved connection established")
+    ,1000})
 })
+
+const promiseAgain=promiseThree.then((data)=>{
+    console.log(data)
+    return "a new promise"
+})
+.then((data)=>{
+    console.log(data)
+})
+console.log(promiseAgain)
